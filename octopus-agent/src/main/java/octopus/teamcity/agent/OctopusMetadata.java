d0b600d0bc88ce2ad9ff96436741cc1e040da2e9
Shannon Lewis <slewis74@gmail.com>
2018-11-23 17:03:49 +1000

    added BuildServerType to the OctopusMetadata

--- octopus-agent/src/main/java/octopus/teamcity/agent/OctopusMetadata.java
+++ octopus-agent/src/main/java/octopus/teamcity/agent/OctopusMetadata.java
@@ -3,5 +3,10 @@ package octopus.teamcity.agent;
 import java.util.List;
 
 public class OctopusMetadata {
+    public String BuildServerType;
     public List<WorkItem> WorkItems;
+
+    public OctopusMetadata() {
+        BuildServerType = "TeamCity";
+    }
 }
