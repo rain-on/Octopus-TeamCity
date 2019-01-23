b56f3731aa913947a3d1c3665eb270cd2653052e
Shannon Lewis <slewis74@gmail.com>
2019-01-23 10:55:52 +1000

    include the IssueTrackerId

--- octopus-agent/src/main/java/octopus/teamcity/agent/JiraCommentParser.java
+++ octopus-agent/src/main/java/octopus/teamcity/agent/JiraCommentParser.java
@@ -20,6 +20,8 @@ public class JiraCommentParser extends CommentParser {
         while (jiraMatcher.find()) {
             final WorkItem workItem = new WorkItem();
             workItem.Id = jiraMatcher.group(0);
+            workItem.IssueTrackerId = "issuetracker-jira";
+            workItem.LinkUrl = "browse/" + jiraMatcher.group(0);
             workItem.LinkText = jiraMatcher.group(0);
 
             buildLogger.message("Located Jira work item " + workItem.Id);
--- octopus-agent/src/main/java/octopus/teamcity/agent/WorkItem.java
+++ octopus-agent/src/main/java/octopus/teamcity/agent/WorkItem.java
@@ -2,5 +2,7 @@ package octopus.teamcity.agent;
 
 public class WorkItem {
     public String Id;
+    public String IssueTrackerId;
+    public String LinkUrl;
     public String LinkText;
 }
