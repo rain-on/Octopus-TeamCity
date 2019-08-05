93f5a5b116603f5a8e0ac87ce08dd72da346a779
Jeff Sharp <sharpjs@users.noreply.github.com>
2019-08-05 10:25:41 -0700

    Fix NPE from #27 in another location.

--- octopus-agent/src/main/java/octopus/teamcity/agent/OctopusDeployReleaseBuildProcess.java
+++ octopus-agent/src/main/java/octopus/teamcity/agent/OctopusDeployReleaseBuildProcess.java
@@ -96,7 +96,7 @@ public class OctopusDeployReleaseBuildProcess extends OctopusBuildProcess {
                 if (wait && deployTo != null && !deployTo.isEmpty()) {
                     commands.add("--progress");
 
-                    if (!deploymentTimeout.isEmpty()) {
+                    if (deploymentTimeout != null && !deploymentTimeout.isEmpty()) {
                         commands.add("--deploymenttimeout");
                         commands.add(deploymentTimeout);
                     }
