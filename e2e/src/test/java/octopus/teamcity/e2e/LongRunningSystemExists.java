8e624cb443d617362bab94267bf94387e82917c0
Trent Mohay <trent.mohay@octopus.com>
2021-06-22 15:09:36 +1000

    Add e2e Tests
    Tests which start up TeamCity in a docker container (both agent and server),
    as well as an OctopusDeploy Server.
    
    A specific, canned, config is injected to the TeamCity, and a build is commenced,
    the test then ensures (via the octopus-java-api) that the OctopusServer has the
    expected documents.
    
    remove line endings

---
+++ e2e/src/test/java/octopus/teamcity/e2e/LongRunningSystemExists.java
@@ -0,0 +1,40 @@
+package octopus.teamcity.e2e;
+
+import org.junit.jupiter.api.Test;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.mock.Expectation;
+import org.mockserver.model.Header;
+import org.mockserver.model.HttpRequest;
+import org.mockserver.model.HttpResponse;
+
+import static com.google.common.net.HttpHeaders.CONTENT_LENGTH;
+import static com.google.common.net.HttpHeaders.HOST;
+
+public class LongRunningSystemExists extends BaseRecordReplay {
+
+  @Test
+  public void runThis() throws InterruptedException {
+    mockServer = createMockServer();
+    while (true) {
+      Thread.sleep(10000);
+    }
+  }
+
+  @Override
+  protected ClientAndServer createMockServer() {
+    System.setProperty("mockserver.initializationJsonPath", generateLogPath().toString());
+    final ClientAndServer clientAndServer = new ClientAndServer(8065);
+
+    for (final Expectation expectation : clientAndServer.retrieveActiveExpectations(null)) {
+      // NOTE: the contentLength is a bit wrong as it doesn't know if it wants /n or not (content length implies NO,
+      //content imoplies yes!
+      final String body = expectation.getHttpResponse().getBody().toString();
+      final HttpResponse modifiedResponse = expectation.getHttpResponse().replaceHeader(CONTENT_LENGTH,
+          Integer.toString(body.length()));
+      expectation.thenRespond(modifiedResponse);
+      clientAndServer.upsert(expectation);
+    }
+
+    return clientAndServer;
+  }
+}
